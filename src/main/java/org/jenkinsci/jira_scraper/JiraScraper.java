875324ec8fd1d30353142f37a68427b8e4a700d6
Ulli Hafner <ullrich.hafner@gmail.com>
2012-04-02 15:00:51 +0200

    Added method to change the default assignee of a component to an existing user.

--- src/main/java/org/jenkinsci/jira_scraper/JiraScraper.java
+++ src/main/java/org/jenkinsci/jira_scraper/JiraScraper.java
@@ -34,18 +34,23 @@ public class JiraScraper {
     }
 
     public void setDefaultAssignee(String projectId, String component, AssigneeType assignee) throws IOException {
+        setDefaultAssignee(projectId, component, assignee, null);
+    }
+
+    public void setDefaultAssignee(String projectId, String component, AssigneeType assignee, String name) throws IOException {
         for (BasicComponent c : restClient.getProjectClient().getProject(projectId,pm).getComponents()) {
             if (c.getName().equals(component)) {
                 Component comp = restClient.getComponentClient().getComponent(c.getSelf(),pm);
-                ComponentInput ci = new ComponentInput(component,c.getDescription(),comp.getLead().getName(),assignee);
+                String componentLead = name == null ? comp.getLead().getName() : name;
+                ComponentInput ci = new ComponentInput(component,c.getDescription(),componentLead,assignee);
                 restClient.getComponentClient().updateComponent(c.getSelf(), ci, pm);
                 return;
             }
         }
-
+        
         throw new IOException("Unable to find component "+component+" in the issue tracker");
     }
-
+    
     // test
     public static void main(String[] args) throws Exception {
         JiraScraper js = new JiraScraper();
